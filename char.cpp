e856a0474d133f4793d2ec23a2a32f40b3f786a3
kyungjunn <1124kyungjun@naver.com>
2026-03-11 14:04:42 +0900

    함수로 바꾸기

--- char.cpp
+++ char.cpp
@@ -2,53 +2,65 @@
 #define Length 10
 using namespace std;
 
-int main()
+// 문자열 갯수 세기
+int CountString(char* String)
 {
-	char String[Length];
-
-	cin >> String;
-
-	// 문자열 갯수 세기
 	for (int i = 0; i < Length; ++i)
 	{
 		if (String[i] == '\0')
 		{
-			cout << "문자열 개수는 " << i << endl;
-			break;
+			return i;
 		}
 	}
-
-	// 문자열 중에서 특정 문자를 특정문자로 바꾸기
+	return Length;
+}
+// 문자열 중에서 특정 문자를 특정문자로 바꾸기
+void ChangeString(char* String, char FChar, char CChar)
+{
+	for (int i = 0; i < Length; ++i)
+	{
+		if (String[i] == FChar)
+		{
+			String[i] = CChar;
+		}
+	}
+	cout << "변경 후 : " << String << endl;
+}
+// 문자열 중에서 특정 문자 위치 찾기
+int LocationString(char* String, char LChar)
+{
+	for (int i = 0; i < Length; ++i)
+	{
+		if (String[i] == LChar)
+		{
+			return i + 1;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	char String[Length];
 	char FindChar;
 	char ChangeChar;
-
-	cout << "문자열 중에서 문자를 바꿀 문자를 입력" << endl;
-	cin >> FindChar;
-
-	for (int i = 0; i < Length; ++i)
-	{
-		if (String[i] == FindChar)
-		{
-			cout << "뭘로 바꿁래" << endl;
-			cin >> ChangeChar;
-
-			String[i] = ChangeChar;
-			cout << String << endl;
-		}
-	}
-
-	// 문자열 중에서 특정 문자 위치 찾기
 	char LocationChar;
 
+	cin >> String;
+
+	// 문자열 길이 출력
+	cout << "문자열 길이는: " << CountString(String) << endl;
+
+	// 특정 문자를 특정 문자로 바꾸기 출력 
+	cout << "문자열 중에서 문자를 바꿀 문자를 입력" << endl;
+	cin >> FindChar >> ChangeChar;
+
+	ChangeString(String, FindChar, ChangeChar);
+
+	// 특정 문자 위치 출력
 	cout << "위치를 찾을 특정 문자 입력 " << endl;
 	cin >> LocationChar;
+	int Location = LocationString(String, LocationChar);
 
-	for (int i = 0; i < Length; ++i)
-	{
-		if (String[i] == LocationChar)
-		{
-			cout << LocationChar << "의 위치는 " << i + 1 << " 번째 입니다." << endl;
-		}
-	}
+	cout << LocationChar << "의 위치는 " << Location << " 번째 입니다." << endl;
 	return 0;
 }
