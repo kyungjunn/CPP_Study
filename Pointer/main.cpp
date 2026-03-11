c46aec2379b1c61b321817baf737490360c4538f
kyungjunn <1124kyungjun@naver.com>
2026-03-11 17:31:35 +0900

    배열 동적 생성 및 출력 예제

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -6,28 +6,33 @@ using namespace std;
 int main()
 {
 	// 숫자 입력받아서 그걸 크기로 숫자 배열을 만들어서 1부터 크기까지 초기화하고 출력하는 프로그램
-
 	int Size;
+	int* Number = nullptr; // 중간에 쓰면 헷갈리니까 선언할 때 비워놓고 채워넣는 느낌으로.
 	cin >> Size;
 
-	int* NumberArray = new int[Size];
+	// heap 배열을 size 크기만큼 int, 시작 번지(int*)
+	Number = new int[Size];
 	
+	if (!Number) // if (Number == nullptr)
+	{
+		return -1;
+	}
+
 	for (int i = 0; i < Size; ++i)
 	{
-		NumberArray[i] = i + 1;
+		Number[i] = i + 1;
 	}
 
-	
-	if (NumberArray)
+	for (int i = 0; i < Size; ++i)
 	{
-		for (int i = 0; i < Size; ++i)
-		{
-			cout << NumberArray[i];
-		}
+		cout << Number[i] << ", ";
 	}
+	cout << endl;
 
-	delete[] NumberArray;
-	NumberArray = nullptr;
+
+	// heap에 있는 배열 반환
+	delete[] Number;
+	Number = nullptr;	// 유효한 값이 아니라고 전달
 
 	return 0;
 }
