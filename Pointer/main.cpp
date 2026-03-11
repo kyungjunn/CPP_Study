49f4ae16338db419569db85d049c87196b9f2791
kyungjunn <1124kyungjun@naver.com>
2026-03-11 17:14:36 +0900

    배열 동적 생성

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -5,12 +5,29 @@ using namespace std;
 
 int main()
 {
-	char Temp[100] = "Hello World";
+	// 숫자 입력받아서 그걸 크기로 숫자 배열을 만들어서 1부터 크기까지 초기화하고 출력하는 프로그램
 
-	int StringLength = Length(Temp, 100);
-	cout << StringLength << endl;
+	int Number;
+	cin >> Number;
 
-	Replace(Temp, 100, 'l', 'f');
+	int* NumberArray = new int[Number];
+	
+	for (int i = 0; i < Number; ++i)
+	{
+		NumberArray[i] = i + 1;
+	}
+
+	
+	if (NumberArray)
+	{
+		for (int i = 0; i < Number; ++i)
+		{
+			cout << NumberArray[i];
+		}
+	}
+
+	delete[] NumberArray;
+	NumberArray = nullptr;
 
 	return 0;
 }
