240c21e0c8727132497959f2df805b9778272cd9
kyungjunn <1124kyungjun@naver.com>
2026-03-12 17:19:13 +0900

    헤더파일을 사용한 맵 만들기

---
+++ Map/Engine.cpp
@@ -0,0 +1,113 @@
+#include "Engine.h"
+#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+
+// 플레이어 위치 초기화
+FVector2i PlayerPosition = { 1, 1 };
+
+int Map[10][10] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+int Input()
+{
+	// _kbhit() : 눌렀을 때 true 아니면 안나오게
+	return _getch();
+}
+void Process(int PlayerInput)
+{
+	int SavePointX = PlayerPosition.X;
+	int SavePointY = PlayerPosition.Y;
+
+	// 플레이어 이동
+	if (PlayerInput == 'W' || PlayerInput == 'w')
+	{
+		AddPlayerOffset(0, -1);
+
+	}
+	if (PlayerInput == 'S' || PlayerInput == 's')
+	{
+		AddPlayerOffset(0, 1);
+	}
+	if (PlayerInput == 'A' || PlayerInput == 'a')
+	{
+		AddPlayerOffset(-1, 0);
+	}
+	if (PlayerInput == 'D' || PlayerInput == 'd')
+	{
+		AddPlayerOffset(1, 0);
+	}
+
+	if (Map[PlayerPosition.Y][PlayerPosition.X] == 1)
+	{
+		PlayerPosition.X = SavePointX;
+		PlayerPosition.Y = SavePointY;
+	}
+}
+
+void Clear()
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			Gotoxy(X, Y);
+			std::cout << " ";
+		}
+	}
+}
+
+void Render()
+{
+	Clear();
+
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			Gotoxy(X, Y);
+
+			if (PlayerPosition.X == X && PlayerPosition.Y == Y)
+			{
+				std::cout << "P";
+			}
+			else if (Map[Y][X] == 1)
+			{
+				std::cout << "#";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				std::cout << " ";
+			}
+			
+		}
+	}
+}
+
+void Gotoxy(int x, int y)
+{
+	COORD Pos = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+void AddPlayerOffset(FVector2i DeltaPosition)
+{
+	PlayerPosition.X += DeltaPosition.X;
+	PlayerPosition.Y += DeltaPosition.Y;
+}
+
+void AddPlayerOffset(int DeltaX, int DeltaY)
+{
+	PlayerPosition.X += DeltaX;
+	PlayerPosition.Y += DeltaY;
+}
--- Map/main.cpp
+++ Map/main.cpp
@@ -1,111 +1,18 @@
-#include <iostream>
-#include <conio.h>
-#define Max 10
-using namespace std;
-
-// 맵 초기화
-int Map[Max][Max] =
-{
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-// 플레이어 초기 위치
-int PlayerX = 1;
-int PlayerY = 1;
-
-// 플레이어 키 입력
-int PlayerInput = 0;
-
-// 게임 진행 상태
-bool bIsPlaying = true;
-
-void Input();
-void Process();
-void Render();
+#include "Engine.h"
 
 int main()
 {
+	bool bIsPlaying = true;
 	Render();
-	
-	while (bIsPlaying)
+
+	// 절차지향
+	// GameLoop
+	while (bIsPlaying) // -> Frame
 	{
-		Input();
-		Process();
+		int PlayerInput = Input();
+		Process(PlayerInput);
 		Render();
 	}
-	cout << "아 줴제ㅜ이야";
 
 	return 0;
-}
-void Input()
-{
-	PlayerInput = _getch();
-}
-void Process()
-{
-	int SavePointX = PlayerX;
-	int SavePointY = PlayerY;
-
-	// 플레이어 이동
-	if (PlayerInput == 'W' || PlayerInput == 'w')
-	{
-		PlayerY--;
-	}
-	else if (PlayerInput == 'S' || PlayerInput == 's')
-	{
-		PlayerY++;
-	}
-	else if (PlayerInput == 'A' || PlayerInput == 'a')
-	{
-		PlayerX--;
-	}
-	else if (PlayerInput == 'D' || PlayerInput == 'd')
-	{
-		PlayerX++;
-	}
-
-	if (Map[PlayerY][PlayerX] == 1)
-	{
-		PlayerX = SavePointX;
-		PlayerY = SavePointY;
-	}
-
-	if (PlayerX == Max - 2 && PlayerY == Max - 2)
-	{
-		bIsPlaying = false;
-	}
-}
-void Render()
-{
-	// 이전 맵 지우기
-	system("cls");
-
-	for (int Y = 0; Y < Max; ++Y)
-	{
-		for (int X = 0; X < Max; ++X)
-		{
-			if (PlayerX == X && PlayerY == Y)
-			{
-				cout << 'P';
-			}
-			else if (Map[Y][X] == 1)
-			{
-				cout << '*';
-			}
-			else if (Map[Y][X] == 0)
-			{
-				cout << ' ';
-			}
-		}
-		cout << '\n';
-	}
 }
