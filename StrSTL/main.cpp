390f7a918e0cf53afd6ae5500f74dbf5cbe17ca1
kyungjunn <1124kyungjun@naver.com>
2026-03-26 17:50:06 +0900

    주석처리

--- StrSTL/main.cpp
+++ StrSTL/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include "StrSTL.h"
 #include "String.h"
 
 using namespace std;
 
 int main()
 {
-	StrSTL String1("qw");
-	StrSTL String2("er");
-	StrSTL String3 = String1 + String2;
-	
-	cout << String3.Data;
-	
 	String s1 = "qwer";
 	String s2 = "asdf";
 	String s3 = s1 + s2;
