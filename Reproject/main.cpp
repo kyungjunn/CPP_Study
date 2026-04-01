72075cb348b3c88820a6151886b82aa48a65bab9
kyungjunn <1124kyungjun@naver.com>
2026-04-01 10:57:25 +0900

    윈도우 창 만들기

--- Reproject/main.cpp
+++ Reproject/main.cpp
@@ -13,6 +13,25 @@ using namespace std;
 
 int SDL_main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* MyWindow = SDL_CreateWindow("Hello", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+	while (1)
+	{
+		SDL_Event MyEvent;
+		SDL_PollEvent(&MyEvent); // 이벤트가 있냐 계속 물어보는거임
+
+		if (MyEvent.type == SDL_QUIT) // 이벤트에서 type이 창 끄는 걸 눌렀냐
+		{
+			break;
+		}
+	}
+
+	SDL_DestroyWindow(MyWindow);
+
+	SDL_Quit();
+
 	return 0;
 }
 
