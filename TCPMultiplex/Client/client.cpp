267d9fa4a9fb4577d700b6a4a3201c7b960b47fc
kyungjunn <1124kyungjun@naver.com>
2026-05-15 14:23:41 +0900

    another Thread

--- TCPMultiplex/Client/client.cpp
+++ TCPMultiplex/Client/client.cpp
@@ -1,32 +1,66 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
+#include <WinSock2.h> // 얘가 나중에 나온거라 include 가 중첩됨. 그래서 먼저 해야됨.
+#include <Windows.h>
 
-#include <WinSock2.h>
 #include <iostream>
 #include <process.h>
 
+#include <atomic> // OS고 뭐고 다 사용 가능
+
 #pragma comment(lib, "ws2_32")
 
-DWORD ThreadFunction(void* Argument)
+std::atomic<unsigned> 내돈 = 10000;
+
+unsigned Increasement(void* Argument)
 {
 	for (int i = 0; i < 10000; ++i)
 	{
-		printf("%d\n", i);
+		// 문이 열려있으면 문닫고
+		// 원자적으로 작업(원자적으로 더하기)
+		// 같은 세줄이지만 이 세줄을 묶어서 뺏기지 않도록
+		//InterlockedIncrement(&내돈); // Windows 전용
+		내돈.fetch_add(1); // atomic
+		//내돈++;
+		// 1. 글로벌 변수 접근(숫자 가져오기) << 여기에서 끊김
+		// 2. 가져온 숫자에 +1
+		// 3. 다시 글로벌 변수에 저장
+		// 문열고
 	}
 	return 0;
 }
 
-int main()
+unsigned Decreasement(void* Argument)
 {
-	LPDWORD lpThreadId = 0;
-
-	// OS님께 부탁하는거임. Thread 함수 알아서 실행 시켜주세요. OS한테 스레드 함수 등록.
-	HANDLE ThreadHandle = CreateThread(0, 0, ThreadFunction, 0, 0, lpThreadId); // lpThreadId -> 0 해도 됨.
-
-	while (true);
+	for (int i = 0; i < 10000; ++i)
+	{
+		//InterlockedDecrement(&내돈);
+		//내돈--;
+		내돈.fetch_sub(1);
+	}
 	return 0;
 }
-int main2()
+
+// race condition
+
+int main()
+{
+	//LPDWORD lpThreadId = 0;
+	//HANDLE ThreadHandle2 = CreateThread(0, 0, ThreadFunction, 0, 0, lpThreadId);
+	HANDLE ThreadHandle1 = (HANDLE)_beginthreadex(0, 0, &Increasement, 0, 0, 0);
+	HANDLE ThreadHandle2 = (HANDLE)_beginthreadex(0, 0, &Decreasement, 0, 0, 0);
+	HANDLE ThreadHandle3 = (HANDLE)_beginthreadex(0, 0, &Increasement, 0, 0, 0);
+	HANDLE ThreadHandle4 = (HANDLE)_beginthreadex(0, 0, &Decreasement, 0, 0, 0);
+
+
+
+	//printf("내 돈은? %d\n", 내돈);
+	std::cout << "내 돈은? " << 내돈 << std::endl;
+
+	return 0;
+}
+
+int main3()
 {
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
